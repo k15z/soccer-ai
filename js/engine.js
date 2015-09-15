5e06ab760399b8d6da08feed7c9c01ce420ef5ea
Kevin Zhang <kevin.2015.zhang@gmail.com>
2015-09-15 09:15:43 -0400

    Inter-robot collisions

--- js/engine.js
+++ js/engine.js
@@ -122,27 +122,37 @@ function Engine(canvas, Player1, Player2) {
                 ball.vy = MAX_SPEED * ball.vy / Math.abs(ball.vy);
             
             var distance = 0.0;
-            for (var i = 0; i < NUM_PLAYERS; i++) {
+            var teams = [].concat(team1).concat(team2);
+            for (var i = 0; i < teams.length; i++) {
                 distance = Math.sqrt(
-                    (team1[i].x - ball.x) * (team1[i].x - ball.x) + 
-                    (team1[i].y - ball.y) * (team1[i].y - ball.y)
+                    (teams[i].x - ball.x) * (teams[i].x - ball.x) + 
+                    (teams[i].y - ball.y) * (teams[i].y - ball.y)
                 );
                 if (distance <= BOT_RADIUS + BALL_RADIUS) {
-                    var newVelX1 = (ball.vx * (BALL_RADIUS - BOT_RADIUS) + (2 * BOT_RADIUS * team1[i].vx)) / (BALL_RADIUS + BOT_RADIUS);
-                    var newVelY1 = (ball.vy * (BALL_RADIUS - BOT_RADIUS) + (2 * BOT_RADIUS * team1[i].vy)) / (BALL_RADIUS + BOT_RADIUS);
+                    var newVelX1 = (ball.vx * (BALL_RADIUS - BOT_RADIUS) + (2 * BOT_RADIUS * teams[i].vx)) / (BALL_RADIUS + BOT_RADIUS);
+                    var newVelY1 = (ball.vy * (BALL_RADIUS - BOT_RADIUS) + (2 * BOT_RADIUS * teams[i].vy)) / (BALL_RADIUS + BOT_RADIUS);
                     ball.vx = 2*newVelX1; ball.vy = 2*newVelY1;
                     ball.x += ball.vx; ball.y += ball.vy;
                 }
-
-                distance = Math.sqrt(
-                    (team2[i].x - ball.x) * (team2[i].x - ball.x) + 
-                    (team2[i].y - ball.y) * (team2[i].y - ball.y)
-                );
-                if (distance <= BOT_RADIUS + BALL_RADIUS) {
-                    var newVelX1 = (ball.vx * (BALL_RADIUS - BOT_RADIUS) + (2 * BOT_RADIUS * team2[i].vx)) / (BALL_RADIUS + BOT_RADIUS);
-                    var newVelY1 = (ball.vy * (BALL_RADIUS - BOT_RADIUS) + (2 * BOT_RADIUS * team2[i].vy)) / (BALL_RADIUS + BOT_RADIUS);
-                    ball.vx = 2*newVelX1; ball.vy = 2*newVelY1;
-                    ball.x += ball.vx; ball.y += ball.vy;
+                
+                for (var j = i; j < teams.length; j++) {
+                    if (j == i)
+                        continue;
+                    distance = Math.sqrt(
+                        (teams[i].x - teams[j].x) * (teams[i].x - teams[j].x) + 
+                        (teams[i].y - teams[j].y) * (teams[i].y - teams[j].y)
+                    );
+                    if (distance <= BOT_RADIUS + BOT_RADIUS) {
+                        teams[i].x -= teams[i].vx;
+                        teams[i].y -= teams[i].vy;
+                        teams[j].x -= teams[j].vx;
+                        teams[j].y -= teams[j].vy;
+                        
+                        teams[i].vx = 0;
+                        teams[i].vy = 0;
+                        teams[j].vx = 0;
+                        teams[j].vy = 0;
+                    }
                 }
             }
         })();
@@ -156,40 +166,25 @@ function Engine(canvas, Player1, Player2) {
                 "team1": team1,
                 "team2": team2
             };
-            for (var i = 0; i < NUM_PLAYERS; i++) {
-                var state2 = $.extend({}, state, team1[i]);
-                var accel = team1[i].player.action(state2);
-                team1[i].vx += accel.ax;
-                if (Math.abs(team1[i].vx) > MAX_SPEED/5)
-                    team1[i].vx = (MAX_SPEED/5) * team1[i].vx / Math.abs(team1[i].vx);
-                team1[i].vy += accel.ay;
-                if (Math.abs(team1[i].vy) > MAX_SPEED/5)
-                    team1[i].vy = (MAX_SPEED/5) * team1[i].vy / Math.abs(team1[i].vy);
-                if (team1[i].x - BOT_RADIUS < 0)
-                    team1[i].vx = 0;
-                if (team1[i].x > FIELD_WIDTH - BOT_RADIUS)
-                    team1[i].vx = 0;
-                if (team1[i].y - BOT_RADIUS < 0)
-                    team1[i].vy = 0;
-                if (team1[i].y > FIELD_HEIGHT - BOT_RADIUS)
-                    team1[i].vy = 0;
-                
-                var state2 = $.extend({}, state, team2[i]);
-                var accel = team2[i].player.action(state2);
-                team2[i].vx += accel.ax;
-                if (Math.abs(team2[i].vx) > MAX_SPEED/5)
-                    team2[i].vx = (MAX_SPEED/5) * team2[i].vx / Math.abs(team2[i].vx);
-                team2[i].vy += accel.ay;
-                if (Math.abs(team2[i].vy) > MAX_SPEED/5)
-                    team2[i].vy = (MAX_SPEED/5) * team2[i].vy / Math.abs(team2[i].vy);
-                if (team2[i].x - BOT_RADIUS < 0)
-                    team2[i].vx = 0;
-                if (team2[i].x > FIELD_WIDTH - BOT_RADIUS)
-                    team2[i].vx = 0;
-                if (team2[i].y - BOT_RADIUS < 0)
-                    team2[i].vy = 0;
-                if (team2[i].y > FIELD_HEIGHT - BOT_RADIUS)
-                    team2[i].vy = 0;
+            
+            var teams = [].concat(team1).concat(team2);
+            for (var i = 0; i < teams.length; i++) {
+                var state2 = $.extend({}, state, teams[i]);
+                var accel = teams[i].player.action(state2);
+                teams[i].vx += accel.ax;
+                if (Math.abs(teams[i].vx) > MAX_SPEED/5)
+                    teams[i].vx = (MAX_SPEED/5) * teams[i].vx / Math.abs(teams[i].vx);
+                teams[i].vy += accel.ay;
+                if (Math.abs(teams[i].vy) > MAX_SPEED/5)
+                    teams[i].vy = (MAX_SPEED/5) * teams[i].vy / Math.abs(teams[i].vy);
+                if (teams[i].x - BOT_RADIUS < 0)
+                    teams[i].vx = 0;
+                if (teams[i].x > FIELD_WIDTH - BOT_RADIUS)
+                    teams[i].vx = 0;
+                if (teams[i].y - BOT_RADIUS < 0)
+                    teams[i].vy = 0;
+                if (teams[i].y > FIELD_HEIGHT - BOT_RADIUS)
+                    teams[i].vy = 0;
             }
         })();
         
